39d42d751c570dc20980b6d6c600a9a76bc72ab0
MrKristto <kristto1911@gmail.com>
2024-07-09 22:46:25 -0500

    Create soccer registration view

--- app/src/main/java/com/example/solarsports/SolarRegistrationSoccerActivity.java
+++ app/src/main/java/com/example/solarsports/SolarRegistrationSoccerActivity.java
@@ -1,6 +1,5 @@
 package com.example.solarsports;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,27 +7,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.Button;
 
-public class SolarRegistrationActivity extends AppCompatActivity {
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SolarRegistrationSoccerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_solar_registration);
+        setContentView(R.layout.activity_solar_registration_soccer);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button buttonSoccer = findViewById(R.id.buttonSoccer);
+        String[] power = {"200W","250W","300W","350W","400W","450W","500W"};
 
-        buttonSoccer.setOnClickListener(v -> {
-            Intent intent = new Intent(SolarRegistrationActivity.this, SolarRegistrationSoccerActivity.class);
-            startActivity(intent);
-        });
+        Spinner spinnerPower = findViewById(R.id.spinnerPower);
+
+        spinnerPower.setAdapter(new ArrayAdapter<>(SolarRegistrationSoccerActivity.this, android.R.layout.simple_spinner_item, power));
 
     }
 }
